6a9522731439cf6b9870ecb156f0a87651d92f51
Wojciech Muszala <wojciech.muszala@gmail.com>
2023-12-11 20:30:31 +0100

    Feature: Add loading spinner to async data

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,11 +6,14 @@
     <base-card>
       <div class="controls">
         <base-button mode="outline" @click="loadCoaches"> Refresh </base-button>
-        <base-button v-if="!isCoach" link to="/register">
+        <base-button v-if="!isCoach && !isLoading" link to="/register">
           Register as Coach
         </base-button>
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <base-spinner></base-spinner>
+      </div>
+      <ul v-else-if="hasCoaches">
         <coach-item
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -37,6 +40,7 @@ export default {
   },
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -61,7 +65,7 @@ export default {
       });
     },
     hasCoaches() {
-      return this.$store.getters["coaches/hasCoaches"];
+      return !this.isLoading && this.$store.getters["coaches/hasCoaches"];
     },
     isCoach() {
       return this.$store.getters["coaches/isCoach"];
@@ -74,8 +78,13 @@ export default {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
     },
-    loadCoaches() {
+    async loadCoaches() {
+      this.isLoading = true;
       this.$store.dispatch("coaches/loadCoaches");
+
+      setTimeout(() => {
+        this.isLoading = false;
+      }, "1000");
     },
   },
 };
