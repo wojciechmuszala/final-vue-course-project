0e6c07df839937e352ec12d2773761df9e1b6b2e
Wojciech Muszala <wojciech.muszala@gmail.com>
2023-12-13 20:07:57 +0100

    Feature: Expand NotFound page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,17 +1,11 @@
 <template>
-  <div>
-    Error 404 Not Found
-
-    
-  </div>
+  <section>
+    <base-card>
+      <h2>Error 404 Not Found</h2>
+      <p>
+        This page could not be found - maybe check out all our
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
 </template>
-
-<script>
-  export default {
-    
-  }
-</script>
-
-<style scoped>
-
-</style>
